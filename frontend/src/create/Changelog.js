ee9217924ed5e5f16bbdac0c25feacef754701a9
Phua Yue Jun <whyjayxp@hotmail.com>
2021-01-10 18:55:42 +0800

    make changelog collapsable

--- frontend/src/create/Changelog.js
+++ frontend/src/create/Changelog.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
-function Changelog() {
+class Changelog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showItems: false
+    };
+  }
+
+  render() {
     return (
-        <div id="changelog">
-          <b>Changelog</b>
-          <ul id="logList">
+        <div id="changelog" onClick={() => (this.state.showItems) ? this.setState({showItems: false}) : this.setState({showItems: true})}>
+          <b>Changelog (click to show/hide)</b>
+          <ul id="logList" style={{ display: (this.state.showItems) ? 'block' : 'none' }}>
             <li><b>v1.2 (10 Jan 2021)</b>: New powerup: Randomized Replace. Powerup/trap probabilities have been modified. Proper Game Over page added to honour the winner.</li>
             <li><b>v1.1 (10 Jan 2021)</b>: Emoji Mode. Up to 9 players. UI/UX has been enhanced. Server connection issues are now handled better. Changelog was added.</li>
             <li><b>v1.0 (9 Jan 2021)</b>: Tic Tac Foe was created. Up to 6 players per room, with powerups (Skip, Remove, Bomb, Curse) and traps (Joker, Bomb Trap).</li>
           </ul>
         </div>
     );
+  }
 }
 
 export default Changelog;
